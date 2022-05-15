8a162217ec2009d31b2ec6945cfad8e97588ccf4
Manoj <manojujn@gmail.com>
2022-05-15 12:31:09 +0530

    updated build

---
+++ backend/public/precache-manifest.4bf40d4a9421efeecb0de1bb2066bfed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b92b53fee3593d4720e1ab3e03437221",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f309dba1eb7956c7a778",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "80597158b6c14646f8cc",
+    "url": "/static/js/2.16a860dd.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.16a860dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f309dba1eb7956c7a778",
+    "url": "/static/js/main.e9bf4257.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
