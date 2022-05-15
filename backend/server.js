dd5c5727e32a70ad339652ff6a656047c381c501
Manoj <manojujn@gmail.com>
2022-05-15 14:40:40 +0530

    updated server

--- backend/server.js
+++ backend/server.js
@@ -90,8 +90,6 @@ app.post("/stock", (req, res, next) => {
   let stock = req.body.stock;
   console.log(req.body.stock);
   request(`https://www.alphavantage.co/query?function=OVERVIEW&symbol=${stock}&apikey=Z0BM2IYIBVNVQDBP`, function (error, response, body) {
-    // console.error('error:', error); // Print the error if one occurred
-    //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
     const data = JSON.parse(body);
     // console.log(data);
     res.send(data);
